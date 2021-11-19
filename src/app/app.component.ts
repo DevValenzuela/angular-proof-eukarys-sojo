c24478064a2d1210fb686e4dc0cd543f7e5185dd
valenzuela21 <vlzdavid12@outlook.com>
2021-11-19 15:57:43 -0500

    Update Proof

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   isCollapsed = false;
+
+  listMenu: any = [
+    {
+      path: '/customer',
+      title: 'Cliente',
+    },{
+      path: '/product',
+      title: 'Productos',
+    }
+  ]
+
 }
