6df372f0c5ffe95bce846ddc08d0ea5de5cbcfbb
valenzuela21 <vlzdavid12@outlook.com>
2021-11-19 15:51:13 -0500

    Update Proof

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,20 +4,6 @@ import {SharedModule} from 'src/app/shared/shared.module';
 
 import {PagesComponent} from './pages.component'
 
-import {DetailsComponent} from "./customer/details/details.component";
-
-
-import {NzTableModule} from 'ng-zorro-antd/table';
-import {NzInputModule} from 'ng-zorro-antd/input';
-import {NzButtonModule} from 'ng-zorro-antd/button';
-import {NzIconModule} from 'ng-zorro-antd/icon';
-import {NzGridModule} from 'ng-zorro-antd/grid';
-import {NzListModule} from 'ng-zorro-antd/list';
-import {NzDescriptionsModule} from 'ng-zorro-antd/descriptions';
-import {NzDividerModule} from 'ng-zorro-antd/divider';
-
-
-import {ProductPipe} from '../pipes/product.pipe';
 
 @NgModule({
   imports: [
